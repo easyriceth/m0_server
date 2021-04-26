670360c41867f5b264a99d3fb712db5cf3ee243f
matichon maneegard <matichonmaneegard@matichons-MacBook-Pro.local>
2021-04-26 14:02:57 +0700

    add dev version

--- server.js
+++ server.js
@@ -76,6 +76,70 @@ app.get('/save_RICE_INFERENCE', (req, res) => {
     }
 })
 
+app.get('/save_RICE_INFERENCE_DEV', (req, res) => {
+
+    let data = req.body
+    if (data?.arguments?.imageURL) {
+        const imageProcess = axios.post("http://103.253.75.254:4001/inference/",
+            {
+                image: data.arguments.imageURL
+            },
+        )
+
+        imageProcess.then((response) => {
+            let inspectData = {}
+            if (response.data) {
+                Object.keys(response.data).map((keys) => {
+                    if (keys == 'whole_grain_kernel') {
+                        inspectData['wholegrain_kernel'] = response.data[keys]
+                    }
+                    if (keys == 'whole_grain_pixel') {
+                        inspectData['wholegrain_pixel'] = response.data[keys]
+                    }
+                    if (keys == 'grains') {
+                        inspectData[keys] = response.data[keys]
+                        if (Array.isArray(inspectData[keys])) {
+                            inspectData[keys].map((grain, idx) => {
+                                if (grain['shape'] == 'whole_grain') {
+                                    inspectData[keys][idx]['shape'] = 'wholegrain'
+                                } else if (grain['shape'] == 'broken_shape') {
+                                    inspectData[keys][idx]['shape'] = 'broken'
+                                }
+                            })
+                        } else {
+                            inspectData[keys] = response.data[keys]
+                        }
+                    } else {
+                        inspectData[keys] = response.data[keys]
+                    }
+                })
+                let argument = {
+                    ...data.arguments,
+                    dataInference: inspectData,
+                }
+                axios.post("https://4skomnp9df.execute-api.ap-southeast-1.amazonaws.com/default/Dev_save_RICE_INFERENCE"
+                    , {
+                        arguments: argument
+                    }
+                ).then((data) => {
+                    res.json(data.data)
+                }).catch(() => {
+                    res.status(400)
+                    res.json('can not save data into database')
+                })
+            } else {
+                res.status(400)
+                res.json('empty result from compute api')
+            }
+        }).catch(() => {
+            res.status(400)
+            res.json('err connection to compute api')
+        })
+    }else{
+        res.status(400)
+        res.json('not found imageURL')
+    }
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
